428520119daf023b517d43f337230f8ceede35a1
Salomon <pedro.salomon@primetals.com>
2022-04-29 14:00:50 +0200

    backend with @nxrocks plugin

---
+++ projects/vat-calculator-backend/src/main/java/com/jsharper/vatcalculator/VatCalculatorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jsharper.vatcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VatCalculatorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VatCalculatorBackendApplication.class, args);
+	}
+
+}
---
+++ projects/vat-calculator-backend/src/test/java/com/jsharper/vatcalculator/VatCalculatorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsharper.vatcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VatCalculatorBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
