2081b46c4c45b9282bdf0a25093483a9f81bec3d
Pedro Antonio Salomon Ocampos <pedro.salomon@gmail.com>
2022-05-12 23:33:25 +0200

    testing springboot restcontroller works

--- projects/vat-calculator-backend/src/main/java/com/jsharper/vatcalculator/VatCalculatorBackendApplication.java
+++ projects/vat-calculator-backend/src/main/java/com/jsharper/vatcalculator/VatCalculatorBackendApplication.java
@@ -1,9 +1,13 @@
 package com.jsharper.vatcalculator;
 
+import com.jsharper.rest.HelloController;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackageClasses = HelloController.class)
 public class VatCalculatorBackendApplication {
 
 	public static void main(String[] args) {
